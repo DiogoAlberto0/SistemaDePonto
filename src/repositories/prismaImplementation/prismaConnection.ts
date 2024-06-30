3e835bdf357c96fbc9b432b1a241c5437ac31ec6
DiogoAlberto0 <dafgo03@gmail.com>
2024-06-30 20:58:31 -0300

    add readme

--- src/repositories/prismaImplementation/prismaConnection.ts
+++ src/repositories/prismaImplementation/prismaConnection.ts
@@ -3,41 +3,4 @@ import { PasswordUtils } from '../../utils/password/implementation/password.util
 
 const prisma = new PrismaClient()
 
-const init = async () => {
-
-    const passUtils = new PasswordUtils()
-    const position = await prisma.position.create({
-        data: {
-            office: 'admin',
-            privillegeLevel: 5
-        }
-    })
-
-    const station = await prisma.station.create({
-        data: {
-            name: 'Diretoria',
-            cnpj: '0000000000',
-            latitude: -1.000,
-            longitude: -1.000
-        }
-    })
-
-    const user = await prisma.user.create({
-        data: {
-            name: 'ADMIN',
-            phone: '123456789',
-            positionId: position.id,
-            stationId: station.id,
-            hash: passUtils.hash('12345')
-        }
-    })
-
-}
-
-try {
-    init()
-        .catch(erro => console.error('Falha ao executar script'))
-} catch (error) {
-    throw new Error('Falha ao criar usuário')
-}
 export { prisma }

1d4d42db38154c2bbad79ded312023df7d54745b
DiogoAlberto0 <dafgo03@gmail.com>
2024-09-25 08:32:50 -0300

    update vercel deploy

--- src/api/index.ts
+++ src/api/index.ts
@@ -3,10 +3,10 @@ import 'dotenv/config'
 import express from 'express'
 
 //routes
-import { userRoutes } from './routes/userRoutes'
-import { stationRoutes } from './routes/stationRoutes'
-import { positionRoutes } from './routes/positionRoutes'
-import { timeSheetRoutes } from './routes/timeSheetRoutes'
+import { userRoutes } from '../routes/userRoutes'
+import { stationRoutes } from '../routes/stationRoutes'
+import { positionRoutes } from '../routes/positionRoutes'
+import { timeSheetRoutes } from '../routes/timeSheetRoutes'
 
 const port = 4000
 const app = express()

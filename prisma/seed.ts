8379861b9ba9219f10fb88e718f2af2c35c86182
DiogoAlberto0 <dafgo03@gmail.com>
2024-09-25 16:47:07 -0300

    update vercel deploy

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,21 +1,43 @@
 // prisma/seed.ts
 
 import { PrismaClient } from '@prisma/client';
-import { PasswordUtils } from '../src/utils/password/implementation/password.utils'
+import { PasswordUtils } from '../api/utils/password/implementation/password.utils'
 
 const prisma = new PrismaClient();
 
 async function main() {
     const passUtils = new PasswordUtils()
-    const position = await prisma.position.create({
-        data: {
+    const position = await prisma.position.upsert({
+        where: {
+            office: 'admin'
+        },
+        create: {
+            office: 'admin',
+            privillegeLevel: 5
+        },
+        update: {
             office: 'admin',
             privillegeLevel: 5
         }
     })
 
-    const station = await prisma.station.create({
-        data: {
+    const station = await prisma.station.upsert({
+        where: {
+
+            name: 'Diretoria',
+            OR: [
+                {
+                    cnpj: '0000000000'
+                }
+            ]
+        },
+        create: {
+            name: 'Diretoria',
+            cnpj: '0000000000',
+            latitude: -1.000,
+            longitude: -1.000
+        },
+        update: {
             name: 'Diretoria',
             cnpj: '0000000000',
             latitude: -1.000,
@@ -23,8 +45,18 @@ async function main() {
         }
     })
 
-    const user = await prisma.user.create({
-        data: {
+    const user = await prisma.user.upsert({
+        where: {
+            phone: '00123456789'
+        },
+        create: {
+            name: 'ADMIN',
+            phone: '00123456789',
+            positionId: position.id,
+            stationId: station.id,
+            hash: passUtils.hash('12345')
+        },
+        update: {
             name: 'ADMIN',
             phone: '00123456789',
             positionId: position.id,

17252167de3a9562eb324848506ba6f55f66625c
DiogoAlberto0 <dafgo03@gmail.com>
2024-09-25 08:29:35 -0300

    update vercel deploy

--- src/index.ts
+++ src/index.ts
@@ -8,14 +8,22 @@ import { stationRoutes } from './routes/stationRoutes'
 import { positionRoutes } from './routes/positionRoutes'
 import { timeSheetRoutes } from './routes/timeSheetRoutes'
 
+const port = 4000
 const app = express()
 
 app.use(express.json())
 
 
+app.get("/", (req, res) => res.send("Express on Vercel"));
+
 app.use(stationRoutes)
 app.use(positionRoutes)
 app.use(timeSheetRoutes)
 app.use(userRoutes)
+
+
+app.listen(port, () => {
+    console.log(`Executando na porta: ${port}`)
+})
 
 export { app }
